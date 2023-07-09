038f0fbd320906d992dec7319550dfbad844f121
osamaline1229 <osama98138139@gmail.com>
2023-07-09 01:16:44 -0700

    delete method

---
+++ LiikedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LikedList
+    {
+        private Node head;
+        public LikedList() 
+        {
+            head = null;
+        }
+
+        public void Insert (int data)
+        {
+            Node newNode = new Node(data);
+
+            //1. Empty Liked List . Head is Null
+            if(head == null)
+            {
+                head = newNode;
+            }
+
+            //2.When item is added at tthe end.
+            else
+            {
+                Node currentNode = head;
+                while(currentNode.next != null)
+                {
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = newNode; 
+            }
+        }
+
+        public void Delete (int data)
+        {
+            //1. Head is null
+            if(head == null)
+            {
+                Console.WriteLine("linked list is empty.");
+            }
+
+            //2.Head is not null
+            else
+            {
+                if(head.data == data) 
+                {
+                    head = head.next; 
+                }
+
+                else
+                {
+                    Node currentNode = head;
+                    Node preNode = null;
+                    while(currentNode.next != null && currentNode.data != data)
+                    {
+                        
+                        preNode = currentNode;
+                        currentNode = currentNode.next;
+                    }
+                    if(currentNode == null)
+                    {
+                        Console.WriteLine("Data not found in the liked list");
+
+                    }
+                    preNode.next = currentNode.next;
+                }
+            }
+        }
+
+        public void Dispaly()
+        {
+            Node node = head;
+            while(node != null)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using LinkedList;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        LikedList likedList = new LikedList();
+        likedList.Insert(10);
+        likedList.Insert(20);
+        likedList.Insert(30);
+        likedList.Insert(40);
+        likedList.Insert(50);
+        likedList.Dispaly();
+
+        likedList.Delete(20);
+        likedList.Delete(30);
+        Console.WriteLine("After Deleting");
+        likedList.Dispaly();
+        Console.WriteLine("Hello, World!");
+    }
+}
